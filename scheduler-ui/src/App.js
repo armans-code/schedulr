393173af48c757a994f2ff0c073ffbce1a617d92
armans-code <arman.kumaraswamy@gmail.com>
2022-10-19 11:25:51 -0400

    dashboard page & components

--- scheduler-ui/src/App.js
+++ scheduler-ui/src/App.js
@@ -6,42 +6,52 @@ import Dashboard from './pages/Dashboard';
 import IntegrationCallback from './pages/IntegrationCallback';
 import Login from './pages/Login';
 import NewPost from './pages/NewPost';
-import Providers from './pages/Providers';
 import Register from './pages/Register';
+import YourPosts from './pages/YourPosts';
 
 function App() {
-	return (
-		<Routes>
-			<Route path='/' element={<Providers />} />
-			<Route path='/login' element={<Login />} />
-			<Route path='/register' element={<Register />} />
-			<Route
-				path='/new-post'
-				element={
-					<PrivateRoute>
-						<NewPost />
-					</PrivateRoute>
-				}
-			/>
-			<Route
-				path='/dashboard'
-				element={
-					<PrivateRoute>
-						<Dashboard />
-					</PrivateRoute>
-				}
-			/>
-			<Route
-				path='/connected-apps'
-				element={
-					<PrivateRoute>
-						<ConnectedApps />
-					</PrivateRoute>
-				}
-			/>
-			<Route path='/integration-success' element={<IntegrationCallback />} />
-		</Routes>
-	);
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+                path="/new-post"
+                element={
+                    <PrivateRoute>
+                        <NewPost />
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/dashboard"
+                element={
+                    <PrivateRoute>
+                        <Dashboard />
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/connected-apps"
+                element={
+                    <PrivateRoute>
+                        <ConnectedApps />
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/your-posts"
+                element={
+                    <PrivateRoute>
+                        <YourPosts />
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/integration-success"
+                element={<IntegrationCallback />}
+            />
+        </Routes>
+    );
 }
 
 export default App;
--- scheduler-ui/src/components/layout/Header.js
+++ scheduler-ui/src/components/layout/Header.js
@@ -3,42 +3,45 @@ import { NavLink } from 'react-router-dom';
 import HeaderLink from './HeaderLink';
 
 function Header() {
-	const handleClick = async () => {
-		// await logout();
-	};
-	return (
-		<nav class='bg-white py-2 md:py-4 border'>
-			<div class='container px-4 mx-auto md:flex md:items-center'>
-				<div class='flex justify-between items-center'>
-					<NavLink to='/' className='font-bold text-blue-600 text-3xl'>
-						Scheduler
-					</NavLink>
-					<button
-						class='border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden'
-						id='navbar-toggle'
-					>
-						<i class='fas fa-bars'></i>
-					</button>
-				</div>
+    const handleClick = async () => {
+        // await logout();
+    };
+    return (
+        <nav class="bg-white py-2 md:py-4 border">
+            <div class="container px-4 mx-auto md:flex md:items-center">
+                <div class="flex justify-between items-center">
+                    <NavLink
+                        to="/dashboard"
+                        className="font-bold text-blue-600 text-3xl"
+                    >
+                        Scheduler
+                    </NavLink>
+                    <button
+                        class="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden"
+                        id="navbar-toggle"
+                    >
+                        <i class="fas fa-bars"></i>
+                    </button>
+                </div>
 
-				<div
-					class='hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0'
-					id='navbar-collapse'
-				>
-					<HeaderLink to='/dashboard'>Dashboard</HeaderLink>
-					<HeaderLink to='/connected-apps'>Connected Apps</HeaderLink>
-					<HeaderLink to='/your-posts'>Your Posts</HeaderLink>
-					<button onClick={handleClick}>
-						{/* <NavLink to='/login'> */}
-						<p className='p-2 lg:mx-4 md:mx-2 text-gray-600 hover:text-indigo-600 transition-colors duration-300 cursor-pointer'>
-							Sign Out
-						</p>
-						{/* </NavLink> */}
-					</button>
-				</div>
-			</div>
-		</nav>
-	);
+                <div
+                    class="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0"
+                    id="navbar-collapse"
+                >
+                    <HeaderLink to="/dashboard">Dashboard</HeaderLink>
+                    {/* <HeaderLink to="/your-posts">Your Posts</HeaderLink> */}
+                    <HeaderLink to="/connected-apps">Connected Apps</HeaderLink>
+                    <button onClick={handleClick}>
+                        {/* <NavLink to='/login'> */}
+                        <p className="p-2 lg:mx-4 md:mx-2 text-gray-600 hover:text-indigo-600 transition-colors duration-300 cursor-pointer">
+                            Sign Out
+                        </p>
+                        {/* </NavLink> */}
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
