5dec592b0870db513cd6414c1d35a4d0cc8f75ec
armans-code <arman.kumaraswamy@gmail.com>
2022-10-22 16:40:06 -0400

    override cors policy

--- scheduler-service/src/main/java/us/congressionalappchallenge/scheduler/service/SchedulerServiceApplication.java
+++ scheduler-service/src/main/java/us/congressionalappchallenge/scheduler/service/SchedulerServiceApplication.java
@@ -2,6 +2,9 @@ package us.congressionalappchallenge.scheduler.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SchedulerServiceApplication {
@@ -10,4 +13,13 @@ public class SchedulerServiceApplication {
 		SpringApplication.run(SchedulerServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
